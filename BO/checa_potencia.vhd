21ea4bba323676da8a9fa6faff57553e0750a837
sammycosta <arminetah@hotmail.com>
2022-01-23 11:05:43 -0300

    checa potência ok

---
+++ BO/checa_potencia.vhd
@@ -0,0 +1,29 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity checa_potencia is
+generic (N : integer);
+port (entB : in std_logic_vector(N-1 downto 0);
+      zeros: out integer range 0 to N-1);
+end checa_potencia;
+architecture estrutura of checa_potencia is
+signal posicao: integer range 0 to N;
+
+begin
+process (entB)
+variable i : integer;
+begin
+	i := N-1;
+  L3: while i >= 0 loop
+		if entB(i) = '1' then
+			posicao <= i;
+			exit L3;
+		end if;
+		i := i - 1;
+   end loop L3;
+end process;
+
+zeros <= posicao;
+
+end estrutura;
